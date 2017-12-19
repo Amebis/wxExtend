1b5739aa34422980ef8a8550c6430108c92d8858
Simon Rozman <simon@rozman.si>
2017-12-19 11:55:24 +0100

    Set version to 1.5.2

--- include/wxex/common.h
+++ include/wxex/common.h
@@ -27,15 +27,15 @@
 ///
 /// wxExtend Version
 ///
-#define wxEXTEND_VERSION        0x01050100
+#define wxEXTEND_VERSION        0x01050200
 
 #define wxEXTEND_VERSION_MAJ    1       ///< wxExtend Major Version
 #define wxEXTEND_VERSION_MIN    5       ///< wxExtend Minor Version
-#define wxEXTEND_VERSION_REV    1       ///< wxExtend Revision
+#define wxEXTEND_VERSION_REV    2       ///< wxExtend Revision
 #define wxEXTEND_VERSION_BUILD  0       ///< wxExtend Build
 
-#define wxEXTEND_VERSION_STR    "1.5.1" ///< wxExtend Displayable Version
-#define wxEXTEND_BUILD_YEAR_STR "2016"  ///< wxExtend Build Year
+#define wxEXTEND_VERSION_STR    "1.5.2" ///< wxExtend Displayable Version
+#define wxEXTEND_BUILD_YEAR_STR "2017"  ///< wxExtend Build Year
 
 #define wxExtendVersion         "15"    ///< wxExtend API Version
 
